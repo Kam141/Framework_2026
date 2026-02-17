ad99ccb604133595b1bbabc9dec56768ef339a2b
Kamila Habiba Putri Ananta <kamilafadhil@gmail.com>
2026-02-17 20:55:25 +0700

    praktikum 1_Tugas 1

---
+++ praktikum_1/my-app/src/pages/about.tsx
@@ -0,0 +1,11 @@
+export default function About() {
+  return (
+    <div style={{ padding: "20px" }}>
+      <h1>About Me</h1>
+
+      <p><b>Nama Mahasiswa:</b> Kamila Habiba Putri Ananta</p>
+      <p><b>NIM:</b> 2341720175</p>
+      <p><b>Program Studi:</b> Teknologi Informasi</p>
+    </div>
+  );
+}
