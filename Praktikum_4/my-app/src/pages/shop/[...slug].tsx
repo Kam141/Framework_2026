e0f6e5428723381d19c0e9268591ecbb2071e4cd
Kamila Habiba Putri Ananta <kamilafadhil@gmail.com>
2026-02-23 21:34:28 +0700

    jobsheet 4_ Langkah 2  (Modifikasi  [...slug].tsx untuk menampilkan nilai query )

--- Praktikum_4/my-app/src/pages/shop/[...slug].tsx
+++ Praktikum_4/my-app/src/pages/shop/[...slug].tsx
@@ -1,13 +1,14 @@
 import { useRouter } from "next/router";
 
 const HalamanToko = () => {
-  const router = useRouter();
-//   const { slug } = router.query;
-  console.log(router);
+//   const router = useRouter();
+  const { query } = useRouter();
+//   console.log(router);
 
   return (
     <div>
       <h1>Halaman Toko</h1>
+        <p>Toko: {` ${query.slug && query.slug[0] + "-" + query.slug[1]}` }</p>
     </div>
   );
 };
