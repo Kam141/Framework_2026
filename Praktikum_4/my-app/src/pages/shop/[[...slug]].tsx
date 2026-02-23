ce7ef46ec0a10a4f3215ac684d3b9b090294a321
Kamila Habiba Putri Ananta <kamilafadhil@gmail.com>
2026-02-23 22:56:07 +0700

    jobsheet 4_ langkah 5 (Menambahkan validasi agar tidak error saat slug kosong)

--- Praktikum_4/my-app/src/pages/shop/[[...slug]].tsx
+++ Praktikum_4/my-app/src/pages/shop/[[...slug]].tsx
@@ -3,16 +3,23 @@ import { useRouter } from "next/router";
 const HalamanToko = () => {
 //   const router = useRouter();
   const { query } = useRouter();
+  const { slug } = query;
 //   console.log(router);
 
   return (
     <div>
       <h1>Halaman Toko</h1>
         {/* <p>Toko: {` ${query.slug && query.slug[0] + "-" + query.slug[1]}` }</p> */}
+        <p>
+          
+            Kategori: {slug ? slug[0] : "Semua Kategori"} 
 
+        </p>
+        
         <p>
             Toko: {Array.isArray(query.slug) ? query.slug.join(" - ") : query.slug}
         </p>
+        
     </div>
   );
 };
