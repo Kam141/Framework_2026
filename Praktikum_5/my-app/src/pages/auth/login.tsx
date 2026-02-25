a9b74834fcbae2ecef7b3d753e35b0c828a4568f
Kamila Habiba Putri Ananta <kamilafadhil@gmail.com>
2026-02-25 11:07:23 +0700

    jobsheet 5_langkah 1 (Global CSS)

---
+++ Praktikum_5/my-app/src/pages/auth/login.tsx
@@ -0,0 +1,25 @@
+import Link from "next/link";
+import {useRouter} from "next/router";
+
+const halamanLogin = () => {
+  const {push} = useRouter();
+  const handlerLogin = () => {
+    localStorage.setItem("isLogin", "true");
+    push("/produk");
+  }
+  return (
+    <div>
+      <h1>Halaman Login</h1>
+
+      {/*Login → Product (imperatif) */}
+      <button onClick={handlerLogin}>Login</button> <br />
+      {/* <button onClick={() => push('/produk') }>Login</button> <br /> */}
+      {/* <button onClick={() => handlerLogin()}>Login</button> <br /> */}
+
+      {/*Login ↔ Register (Link)  */}
+      {/* <Link href="/auth/register">Ke Halaman Register</Link> */}
+    </div>
+  );
+};
+
+export default halamanLogin;
