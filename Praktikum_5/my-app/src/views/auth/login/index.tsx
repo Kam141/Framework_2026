4b86cea6cedafbf1549b179f2aab5cb6f00cef40
Kamila Habiba Putri Ananta <kamilafadhil@gmail.com>
2026-02-25 22:01:02 +0700

    jobsheet 5_langkah 6 (Inline Styling (CSS-in-JS))

--- Praktikum_5/my-app/src/views/auth/login/index.tsx
+++ Praktikum_5/my-app/src/views/auth/login/index.tsx
@@ -11,15 +11,16 @@ const TampilanLogin = () => {
   }
   return (
     <div className={styles.login}>
-      <h1>Halaman Login</h1>
+      <h1 >Halaman Login</h1>
 
       {/*Login → Product (imperatif) */}
-      {/* <button onClick={handlerLogin}>Login</button> <br /> */}
+      <button onClick={handlerLogin}>Login</button> <br />
       {/* <button onClick={() => push('/produk') }>Login</button> <br /> */}
-      <button onClick={() => handlerLogin()}>Login</button> <br />
+      <h1 style={{ color: "red",border: "1px solid red",borderRadius: "5px",padding: "5px",}}> Belum Punya Akun</h1>
+      {/* <button onClick={() => handlerLogin()}>Login</button> <br /> */}
 
       {/*Login ↔ Register (Link)  */}
-      {/* <Link href="/auth/register">Ke Halaman Register</Link> */}
+      <Link href="/auth/register">Ke Halaman Register</Link>
     </div>
   );
 };
