90579cbf74e017bf9271d5f2c14204401542c657
Kamila Habiba Putri Ananta <kamilafadhil@gmail.com>
2026-03-02 02:17:00 +0700

    jobsheet 6_Tugas 2

--- Praktikum_6/my-app/src/pages/404.tsx
+++ Praktikum_6/my-app/src/pages/404.tsx
@@ -5,8 +5,9 @@ const Custom404 = () => {
     <div className={styles.error}>
             <title>404</title>
       <img src="/page-not-found.png" alt="404" className={styles.error__image} />
-      <h1>404 - Halaman Tidak Ditemukan</h1>
-      <p>Maaf, halaman yang Anda cari tidak ada.</p>
+
+      <h1 className={styles.error__title}>404 - Halaman Tidak Ditemukan</h1>
+      <p className={styles.error__desc}>Maaf, halaman yang Anda cari tidak ada.</p>
     </div>
   )
 }
