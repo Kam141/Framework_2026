86d20effb7a617b3b7199b691701dbf3c47c7782
Kamila Habiba Putri Ananta <kamilafadhil@gmail.com>
2026-03-02 02:23:12 +0700

    jobsheet 6_Tugas 3

--- Praktikum_6/my-app/src/pages/404.tsx
+++ Praktikum_6/my-app/src/pages/404.tsx
@@ -1,9 +1,14 @@
 import styles from '@/styles/404.module.scss'
+import Link from 'next/link'
 
 const Custom404 = () => {
   return (
     <div className={styles.error}>
             <title>404</title>
+
+      <Link href="/" className={styles.back}>
+         ← Kembali ke Home
+      </Link>      
       <img src="/page-not-found.png" alt="404" className={styles.error__image} />
 
       <h1 className={styles.error__title}>404 - Halaman Tidak Ditemukan</h1>
