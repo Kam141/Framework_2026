49ac6cd12704e95e7ae4ef57e0bfa2a729626b04
Kamila Habiba Putri Ananta <kamilafadhil@gmail.com>
2026-03-03 21:29:55 +0700

    jobsheet 7_Langkah 2 (Membuat API Produk)

---
+++ Praktikum_7/my-app/src/pages/auth/register.tsx
@@ -0,0 +1,12 @@
+import Link from "next/link";
+import TampilanRegister from "../../views/auth/register";
+
+const halamanRegister = () => {
+  return (
+    <div>
+      <TampilanRegister />
+    </div>
+  );
+};
+
+export default halamanRegister;
---
+++ Praktikum_7/my-app/src/pages/views/produk/index.tsx
@@ -0,0 +1,9 @@
+const ProductView = () => {
+  return (
+    <div>
+      <h1>Daftar Produk</h1>
+      {/* Konten produk Anda */}
+    </div>
+  );
+};
+export default ProductView;
