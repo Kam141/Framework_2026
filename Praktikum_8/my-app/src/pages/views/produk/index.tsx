32532ad9a9a1c7459a67ac3b80f77d8d7e160696
Kamila Habiba Putri Ananta <kamilafadhil@gmail.com>
2026-03-04 09:42:37 +0700

    jobsheet 8_langkah 3 (Implementasi Skeleton Loading bagian 1)

--- Praktikum_8/my-app/src/pages/views/produk/index.tsx
+++ Praktikum_8/my-app/src/pages/views/produk/index.tsx
@@ -1,5 +1,6 @@
 import styles from "@/pages/produk/produk.module.scss";
 
+
 type ProductType = {
   id: string;
   name: string;
@@ -14,17 +15,10 @@ const TampilanProduk = ({ products }: { products: ProductType[] }) => {
       <h1 className={styles.produk__title}>Daftar Produk</h1>
 
       <div className={styles.produk__content}>
-        {products.map((products: ProductType) => (
-          <div
-            key={products.id}
-            className={styles.produk__content__item}
-          >
+        {/* {products.map((products: ProductType) => (
+          <div key={products.id} className={styles.produk__content__item}>
             <div className={styles.produk__content__item__image}>
-              <img
-                src={products.image}
-                alt={products.name}
-                width={200}
-              />
+              <img src={products.image} alt={products.name} width={200} />
             </div>
 
             <h4 className={styles.produk__content__item__name}>
@@ -39,7 +33,14 @@ const TampilanProduk = ({ products }: { products: ProductType[] }) => {
               Rp {products.price.toLocaleString()}
             </p>
           </div>
-        ))}
+        ))} */}
+
+        <div className={styles.produk__content__skeleton}>
+          <div className={styles.produk__content__skeleton__image}></div>
+          <div className={styles.produk__content__skeleton__name}></div>
+          <div className={styles.produk__content__skeleton__category}></div>
+          <div className={styles.produk__content__skeleton__price}></div>
+        </div>
       </div>
     </div>
   );
