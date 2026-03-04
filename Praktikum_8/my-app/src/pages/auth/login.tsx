b15bcaa75f474c153371b5d56f753c35f4e9fc21
Kamila Habiba Putri Ananta <kamilafadhil@gmail.com>
2026-03-04 09:11:17 +0700

    jobsheet 8_Langkah 1 (Set up data produk)

---
+++ Praktikum_8/my-app/src/pages/auth/login.tsx
@@ -0,0 +1,14 @@
+// import Link from "next/link";
+// import {useRouter} from "next/router";
+import TampilanLogin from "@/pages/views/auth/login";
+
+const halamanLogin = () => {
+  
+  return (
+    <div>
+      <TampilanLogin />
+    </div>
+  );
+};
+
+export default halamanLogin;
---
+++ Praktikum_8/my-app/src/pages/produk/index.tsx
@@ -0,0 +1,56 @@
+import { useRouter } from "next/router";
+import { useEffect, useState } from "react";
+import styles from "./produk.module.css";
+
+type ProductType = {
+  id: string;
+  category: string;
+  image: string;
+  name: string;
+  price: number;
+};
+
+const kategori = () => {
+  // const [isLogin, setIsLogin] = useState(false);
+  // const { push } = useRouter();
+
+  const [products, setProducts] = useState<ProductType[]>([]);
+
+  const fetchProducts = async () => {
+  try {
+    const response = await fetch("/api/produk");
+    const responsedata = await response.json();
+    setProducts(responsedata.data);
+  } catch (error) {
+    console.error("Error fetching produk:", error);
+  }
+};
+
+  useEffect(() => {
+    fetchProducts();
+  }, []);
+  
+  return (
+    <div className={styles.container}>
+      <h1 className={styles.title}>Daftar Produk</h1>
+
+      <button onClick={fetchProducts} className={styles.button}>
+        Refresh Data
+      </button>
+
+      <div className={styles.grid}>
+        {products.map((product) => (
+          <div key={product.id} className={styles.card}>
+            <h2 className={styles.productName}>{product.name}</h2>
+            <p>Kategori: {product.category}</p>
+            <p>Harga: Rp {product.price}</p>
+            <p>Ukuran: {product.image}</p>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+  
+};
+
+export default kategori;
