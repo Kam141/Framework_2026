0e278e46b2d2bced72cc65dcbc397b4a4919402c
Kamila Habiba Putri Ananta <kamilafadhil@gmail.com>
2026-03-06 22:53:24 +0700

    jobsheet 9_Langjkah 1 (Setup Halaman SSR)

---
+++ Praktikum_9/my-app/src/pages/produk/server.tsx
@@ -0,0 +1,12 @@
+import TampilkanProduk from "@/pages/views/produk";
+
+const HalamanProdukServer = () => {
+  return (
+    <div>
+      <h1>Halaman Produk Server</h1>
+      <TampilkanProduk products={[]} />
+    </div>
+  );
+};
+
+export default HalamanProdukServer;
