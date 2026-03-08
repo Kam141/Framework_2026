84e65ba959022967e8aac48a1665a0c12864f0c9
Kamila Habiba Putri Ananta <kamilafadhil@gmail.com>
2026-03-09 00:48:54 +0700

    jobsheet 11_Langkah 2 (Implementasi CSR (Client Rendering))

--- Praktikum_11/my-app/src/pages/produk/[product].tsx
+++ Praktikum_11/my-app/src/pages/produk/[product].tsx
@@ -1,16 +1,24 @@
+import fetcher from "@/utils/swr/fetcher";
 import { useRouter } from "next/router";
+import useSWR from "swr";
 
-const ProdukDetail = () => {
-//   const router = useRouter();
+const HalamanProduk = () => {
+  // const Router = useRouter();
+  // console.log(Router);
+
   const { query } = useRouter();
-// console.log(router);
+
+  const { data, error, isLoading } = useSWR(
+    `/api/products/${query.product}`,
+    fetcher
+  );
 
   return (
     <div>
-      <h1>Detail Produk</h1>
-      <p>Produk:  {query.id}</p>
+      <h1>Halaman Produk</h1>
+      <p>Produk: {query.product}</p>
     </div>
   );
 };
 
-export default ProdukDetail;
+export default HalamanProduk;
