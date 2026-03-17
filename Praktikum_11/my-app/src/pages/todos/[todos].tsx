c4a4844bcfeb7c5db62c34e6d51d8d9b66425932
Kamila Habiba Putri Ananta <kamilafadhil@gmail.com>
2026-03-17 19:57:47 +0700

    js 11_ Tugas SSR

--- Praktikum_11/my-app/src/pages/todos/[todos].tsx
+++ Praktikum_11/my-app/src/pages/todos/[todos].tsx
@@ -4,27 +4,65 @@ import useSWR from "swr";
 import DetailTodo from "@/views/detailTodos";
 import { TodoType } from "@/types/todos.type";
 
-const HalamanTodo = () => {
-  const { query } = useRouter();
+// const HalamanTodo = () => {
+//   const { query } = useRouter();
 
-  const { data, error, isLoading } = useSWR(
-    query.todos ? `/api/todos/${query.todos}` : null,
-    fetcher
-  );
+//   const { data, error, isLoading } = useSWR(
+//     query.todos ? `/api/todos/${query.todos}` : null,
+//     fetcher
+//   );
 
-  if (isLoading) {
-    return <div>Loading...</div>;
-  }
+//   if (isLoading) {
+//     return <div>Loading...</div>;
+//   }
 
-  if (error) {
-    return <div>Gagal mengambil data todo</div>;
+//   if (error) {
+//     return <div>Gagal mengambil data todo</div>;
+//   }
+
+//   return (
+//     <div>
+//       <DetailTodo todos={data?.data?.[0]} />
+//     </div>
+//   );
+// };
+
+// export default HalamanTodo;
+
+
+// server side rendering
+// type Props = {
+//   todos: TodoType | null;
+// };
+
+const HalamanTodo = ({ todos }: {todos: TodoType}) => {
+  if (!todos) {
+    return <div>Data todo tidak ditemukan</div>;
   }
 
   return (
     <div>
-      <DetailTodo todos={data?.data?.[0]} />
+      <DetailTodo todos={todos} />
     </div>
   );
 };
 
 export default HalamanTodo;
+
+export async function getServerSideProps({
+  params,
+}: {
+  params: { todos: string };
+}) {
+
+  const res = await fetch(`http://localhost:3000/api/todos/${params?.todos}`);
+
+
+    const response = await res.json();
+
+    return {
+      props: {
+        todos: response.data?.[0], // Pastikan memberikan nilai default jika data tidak tersedia
+      },
+    };
+}
