f5dac08c2cb4f25b70ed5c10a21789d694894c4d
Kamila Habiba Putri Ananta <kamilafadhil@gmail.com>
2026-03-26 15:16:23 +0700

    js 12_ langkah 1 (Menambahkan  revalidate)

---
+++ Praktikum_12/my-app/src/pages/todos/index.tsx
@@ -0,0 +1,25 @@
+import { useRouter } from "next/router";
+import { useEffect, useState } from "react";
+import  TampilanList from "@/views/todos"
+import useSWR from "swr";
+import fetcher from "@/utils/swr/fetcher";
+
+
+const list = () => {
+  // const [isLogin, setIsLogin] = useState(false);
+  // const { push } = useRouter();
+
+  const [todos, setTodos] = useState([]);
+
+
+  const {data, error, isLoading} = useSWR("/api/todos/", fetcher);
+  
+  return (
+    <div>
+      <TampilanList todos={isLoading ? [] : data?.data || []} />
+    </div>
+  );
+  
+};
+
+export default list;
