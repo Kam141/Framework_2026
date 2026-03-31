c61366ac9074d5bd2eb25fba2c779bca7a901987
Kamila Habiba Putri Ananta <kamilafadhil@gmail.com>
2026-03-31 17:51:02 +0700

    js 13_bagian 2

---
+++ Praktikum_13/my-app/src/views/auth/login/index.tsx
@@ -0,0 +1,28 @@
+import Link from "next/link";
+import {useRouter} from "next/router";
+// import styles from "./login.module.css";
+import styles from "@/views/auth/login/login.module.scss";
+
+const TampilanLogin = () => {
+  const {push} = useRouter();
+  const handlerLogin = () => {
+    localStorage.setItem("isLogin", "true");
+    push("/produk");
+  }
+  return (
+    <div className={styles.login}>
+      <h1 className="text-3xl font-bold text-blue-600">Halaman Login</h1>
+
+      {/*Login → Product (imperatif) */}
+      <button onClick={handlerLogin}>Login</button> <br />
+      {/* <button onClick={() => push('/produk') }>Login</button> <br /> */}
+      <h1 style={{ color: "red",border: "1px solid red",borderRadius: "5px",padding: "5px",}}> Belum Punya Akun</h1>
+      {/* <button onClick={() => handlerLogin()}>Login</button> <br /> */}
+
+      {/*Login ↔ Register (Link)  */}
+      <Link href="/auth/register">Ke Halaman Register</Link>
+    </div>
+  );
+};
+
+export default TampilanLogin;
