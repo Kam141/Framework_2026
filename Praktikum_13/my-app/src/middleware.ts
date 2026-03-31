6ec469b782d5c677ac22387670871dc8f597f594
Kamila Habiba Putri Ananta <kamilafadhil@gmail.com>
2026-03-31 17:52:24 +0700

    js 13_bagian 4 (Batasi Route tertentu)

--- Praktikum_13/my-app/src/middleware.ts
+++ Praktikum_13/my-app/src/middleware.ts
@@ -2,7 +2,11 @@ import { NextResponse } from 'next/server'
 import type { NextRequest } from 'next/server'
 
 export function middleware(request: NextRequest) {
-  return NextResponse.redirect(new URL("/"));
+  return NextResponse.redirect(new URL("/", request.url));
 
     // return NextResponse.next();
+}
+
+export const config = {
+  matcher: ["/produk", "/about"],
 }
