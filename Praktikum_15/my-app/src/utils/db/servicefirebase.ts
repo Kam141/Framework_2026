29deb4232dded80986edd5e25b3941e8bf78c6d3
Kamila Habiba Putri Ananta <kamilafadhil@gmail.com>
2026-04-02 16:27:02 +0700

    js 15_langkah 2 (Membuat API Register)

--- Praktikum_15/my-app/src/utils/db/servicefirebase.ts
+++ Praktikum_15/my-app/src/utils/db/servicefirebase.ts
@@ -1,31 +1,64 @@
-import { 
-  getFirestore, 
-  collection, 
-  getDocs, 
-  getDoc, 
-  doc 
+import {
+  getFirestore,
+  collection,
+  getDocs,
+  Firestore,
+  getDoc,
+  doc,
+  query,
+  addDoc,
+  where,
 } from "firebase/firestore";
 import app from "./firebase";
 
 const db = getFirestore(app);
 
-// mengambil semua data dari collection
 export async function retrieveProducts(collectionName: string) {
   const snapshot = await getDocs(collection(db, collectionName));
-
   const data = snapshot.docs.map((doc) => ({
     id: doc.id,
     ...doc.data(),
   }));
-
   return data;
 }
 
-// mengambil data berdasarkan ID
 export async function retrieveDataByID(collectionName: string, id: string) {
   const snapshot = await getDoc(doc(db, collectionName, id));
-
   const data = snapshot.data();
-
   return data;
+}
+
+export async function signUp(
+  userData: {
+    email: string;
+    fullname: string;
+    password: string;
+  },
+  callback: Function,
+) {
+  const q = query(
+    collection(db, "users"),
+    where("email", "==", userData.email),
+  );
+  const querySnapshot = await getDocs(q);
+  const data = querySnapshot.docs.map((doc) => ({
+    id: doc.id,
+    ...doc.data(),
+  }));
+  // console.log("Query result:", data);
+
+  if (data.length === 0) {
+    // user belum ada -> boleh daftar
+    // await addDoc(collection(db, "users"), userData);
+    // console.log("User registered:", data);
+    callback({
+      status: "success",
+      message: "User registered successfully",
+    });
+  } else {
+    callback({
+      status: "error",
+      message: "User already exists",
+    });
+  }
 }
