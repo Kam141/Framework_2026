fbeea4b8221e7f9eb71c6e1cb5b585f484502e63
Kamila Habiba Putri Ananta <kamilafadhil@gmail.com>
2026-04-03 21:09:23 +0700

    js 16 - bagian 1 (Custom Login Page)

---
+++ Praktikum_16/my-app/src/pages/todos/server.tsx
@@ -0,0 +1,28 @@
+import TampilanProduk from "@/views/todos";
+import { TodoType } from "../../types/todos.type";
+
+
+const halamanTodoServer = (props:{todos:TodoType[]}) => {
+  const { todos } = props;
+  return (
+    <div>
+      <h1>Halaman Produk Server</h1>
+      <TampilanProduk todos={todos} />
+    </div>
+  );
+};
+
+export default halamanTodoServer;
+
+// Fungsi getServerSideProps akan dipanggil setiap kali halaman ini diakses, dan akan mengambil data produk dari API sebelum merender halaman
+export async function getServerSideProps() {
+  const res = await fetch("http://localhost:3000/api/todos");
+  const respone = await res.json();
+
+//   console.log("Data produk yang diambil dari API:", respone);
+  return {
+    props: {
+      todos: respone.data, // Pastikan untuk memberikan nilai default jika data tidak tersedia
+    },
+  };
+}
