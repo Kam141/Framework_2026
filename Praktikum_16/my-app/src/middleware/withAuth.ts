b22cf9352b1bee9eaa87d850b4ac141bb6d2c813
Kamila Habiba Putri Ananta <kamilafadhil@gmail.com>
2026-04-04 19:40:47 +0700

    js 16 - bagian 5 (Calback URL logic)

--- Praktikum_16/my-app/src/middleware/withAuth.ts
+++ Praktikum_16/my-app/src/middleware/withAuth.ts
@@ -13,9 +13,11 @@ export default function withAuth(
         req,
         secret: process.env.NEXTAUTH_SECRET,
       });
+
       if (!token) {
-        const loginUrl = new URL("/", req.url);
-        return NextResponse.redirect(loginUrl);
+        const url = new URL("/auth/login", req.url);
+        url.searchParams.set("callbackUrl", encodeURI(req.url));
+        return NextResponse.redirect(url);
       }
     }
     return middleware(req, next);
