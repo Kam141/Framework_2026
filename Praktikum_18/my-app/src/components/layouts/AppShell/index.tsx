0f22c226578e4459a0ec3f0eafa348d6261a0502
Kamila Habiba Putri Ananta <kamilafadhil@gmail.com>
2026-04-06 15:58:05 +0700

    js 18 - Tugas 4

--- Praktikum_18/my-app/src/components/layouts/AppShell/index.tsx
+++ Praktikum_18/my-app/src/components/layouts/AppShell/index.tsx
@@ -1,32 +1,53 @@
-import {useRouter} from "next/router";
-import Navbar from "../navbar";
-import Footer from "../footer";
+import { useRouter } from "next/router";
+import dynamic from "next/dynamic";
 import { Roboto } from "next/font/google";
+import Script from "next/script";
 
+// Dynamic import
+const Navbar = dynamic(() => import("../navbar"), {
+  loading: () => <p>Loading Navbar...</p>,
+});
+
+const Footer = dynamic(() => import("../footer"), {
+  loading: () => <p>Loading Footer...</p>,
+});
 
 const disableNavbar = ["/auth/login", "/auth/register", "/404"];
 
 type AppShellProps = {
   children: React.ReactNode;
-}
+};
 
 const roboto = Roboto({
   subsets: ["latin"],
   weight: ["400", "500", "700"],
 });
 
-const AppShell = (props:AppShellProps) => {
+const AppShell = (props: AppShellProps) => {
   const { children } = props;
-  const {pathname} = useRouter();
-  // console.log(pathname);
-  return (
-  <main className={roboto.className}>
-    {/* <Navbar /> */}
-    {!disableNavbar.includes(pathname) && <Navbar />}
-    {children}
-    <Footer />
-  </main>
+  const { pathname } = useRouter();
 
+  return (
+    <main className={roboto.className}>
+      
+      {/* Google Analytics */}
+      <Script
+        src="https://www.googletagmanager.com/gtag/js?id=G-NQHQKRW7Q8"
+        strategy="afterInteractive"
+      />
+      <Script id="google-analytics" strategy="afterInteractive">
+        {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+          gtag('config', 'G-NQHQKRW7Q8');
+        `}
+      </Script>
+
+      {!disableNavbar.includes(pathname) && <Navbar />}
+      {children}
+      <Footer />
+    </main>
   );
 };
 
--- Praktikum_18/my-app/src/pages/_app.tsx
+++ Praktikum_18/my-app/src/pages/_app.tsx
@@ -8,21 +8,6 @@ import Script from "next/script";
 export default function App({ Component, pageProps: { session, ...pageProps } }: AppProps) {
   return (
     <SessionProvider session={pageProps.session}>
-
-       {/* Google Analytics */}
-      <Script
-        src="https://www.googletagmanager.com/gtag/js?id=G-NQHQKRW7Q8"
-        strategy="afterInteractive"
-      />
-      <Script id="google-analytics" strategy="afterInteractive">
-        {`
-          window.dataLayer = window.dataLayer || [];
-          function gtag(){dataLayer.push(arguments);}
-          gtag('js', new Date());
-          gtag('config', 'G-NQHQKRW7Q8');
-        `}
-      </Script>
-
       <AppShell>
         <Component {...pageProps} />
       </AppShell>
